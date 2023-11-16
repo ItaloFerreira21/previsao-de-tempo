136f4a26ce8740fffb4bb920b826b7e0cb989bc4
ItaloFerreira21 <134016682+ItaloFerreira21@users.noreply.github.com>
2023-11-16 18:09:43 -0300

    pronto

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function colocarDadosNaTela (dados){
     document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
     document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) +"°C"
     document.querySelector(".previsao").innerHTML = dados.weather[0].description
-    document.querySelector(".umidade").innerHTML =  dados.main.humidity + "%"
+    document.querySelector(".umidade").innerHTML =  dados.main.humidity + "% De umidade"
     document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
 }
 
